71a22f5f34c97d21eca606f064e91d574592c9be
messaoudi <soukaina.messaoudi@etu.umontpellier.fr>
2021-08-23 19:45:40 +0200

    Etape 4

--- src/app/game-console.interface.ts
+++ src/app/game-console.interface.ts
@@ -2,4 +2,7 @@ export interface GameConsole {
   name: string
   sales2017: number
   sales2018: number
+  average2018: number
+  average2019: number
+
 }
--- src/app/services/game-console.service.ts
+++ src/app/services/game-console.service.ts
@@ -10,17 +10,25 @@ export class GameConsoleService {
       {
         name: 'Playstation 4',
         sales2017: 20,
-        sales2018: 18
+        sales2018: 18,
+        average2018: 0.3,
+        average2019: 0.6
+        
       },
       {
         name: 'X Box One',
         sales2017: 7.62,
-        sales2018: 6.96
+        sales2018: 6.96,
+        average2018: 0.1,
+        average2019: 0.5
+
       },
       {
         name: 'Switch',
         sales2017: 0,
-        sales2018: 16.33
+        sales2018: 16.33,
+        average2018: 0.3,
+        average2019: 0.06
       }
     ]
   }
